e3e6f4911ba3a6d006798a63baef690409cc0a15
AhnafMasud1234 <ahnafmasud31@gmail.com>
2025-11-21 10:34:18 +0100

    Redirect FastAPI root '/' to /api/items to show items by default

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -2,6 +2,8 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
 from app.fastapi_api import router as api_router
 
 app = FastAPI(
@@ -19,9 +21,18 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,       # which frontends are allowed
     allow_credentials=True,
-    allow_methods=["*"],         # allow all HTTP methods (GET, POST, PUT, DELETE, ...)
+    allow_methods=["*"],         # allow all HTTP methods
     allow_headers=["*"],         # allow all headers
 )
+
+
+# ---- ROOT ROUTE ----
+# When someone opens http://127.0.0.1:8000/,
+# they will be redirected to /api/items and see the items list.
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/api/items")
+
 
 # ---- API ROUTES ----
 app.include_router(api_router)
